e9e966a368ec44a0b380c39d9a67ff8a3f9840d9
anandhu-as <anandhuas010203@gmail.com>
2023-07-09 19:38:21 +0530

    adding reducers

--- src/Components/TodoList/Todo.jsx
+++ src/Components/TodoList/Todo.jsx
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { clear } from "../../Redux/features/Todo/TodoSlice";
-import { addTodo } from "../../Redux/features/Todo/TodoSlice";
+import {
+  clear,
+  deleteTodo,
+  addTodo,
+} from "../../Redux/features/Todo/TodoSlice";
+
 const Todo = () => {
-  const [inputvalue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
   const { todos } = useSelector((state) => state.todo);
   const dispatch = useDispatch();
+
   const handleAdd = () => {
-    if (inputvalue !== "") {
-      dispatch(addTodo(inputvalue));
+    if (inputValue !== "") {
+      const newTodo = {
+        id: Date.now(),
+        title: inputValue,
+      };
+      dispatch(addTodo(newTodo));
     }
-    return setInputValue("");
+    setInputValue("");
   };
+
   return (
     <>
       <div className="container">
@@ -19,20 +29,25 @@ const Todo = () => {
           type="text"
           placeholder="add notes"
           onChange={(event) => setInputValue(event.target.value)}
-          value={inputvalue}
+          value={inputValue}
         />
-        <button onClick={() => handleAdd()}>add</button>
+        <button onClick={handleAdd}>add</button>
         <div className="todos">
           {todos.map((item) => {
             return (
-              <div className="todo-items">
-                <h3>{item} </h3>
+              <div className="todo-items" key={item.id}>
+                <h3>{item.title}</h3>
+                <button onClick={() => dispatch(deleteTodo({ id: item.id }))}>
+                  delete
+                </button>
               </div>
             );
           })}
         </div>
       </div>
-      <button onClick={() => dispatch(clear())} className="clear">clear</button>
+      <button onClick={() => dispatch(clear())} className="clear">
+        clear
+      </button>
     </>
   );
 };
--- src/Redux/features/Todo/TodoSlice.js
+++ src/Redux/features/Todo/TodoSlice.js
@@ -4,13 +4,19 @@ const TodoSlice = createSlice({
   name: "todo",
   initialState: { todos: [] },
   reducers: {
+    //reducers
     addTodo: (state, action) => {
       state.todos.push(action.payload);
     },
     clear: (state) => {
       state.todos = [];
     },
+    deleteTodo: (state, action) => {
+      const ID = action.payload.id;
+      state.todos = state.todos.filter((item) => item.id !== ID);
+    },
   },
 });
+
 export default TodoSlice.reducer;
-export const { addTodo, clear } = TodoSlice.actions;
+export const { addTodo, clear, deleteTodo } = TodoSlice.actions;
